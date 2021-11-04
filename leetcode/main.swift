0fc508d7044c60fa78712718db63f9a3fd3005e1
leiyu <leiyu@fenbi.com>
2021-11-04 11:07:36 +0800

    first commit

---
+++ leetcode/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  leetcode
+//
+//  Created by Yu Lei on 2020/9/2.
+//  Copyright © 2020 Yu Lei. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!") 
+let i = Solution_6.init().convert("PAYPALISHIRING", 3);
+NSLog("%d", i);
+
