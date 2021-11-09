129df018ec68c35744f324ff69c17feb385639ae
leerme <642459351@qq.com>
2021-11-09 18:12:24 +0800

    509斐波那契数

--- leetcode/main.swift
+++ leetcode/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 print("Hello, World!") 
-let i = Solution_392.init().isSubsequence("abc", "ahbgdc");
-let j = Solution_392.init().isSubsequence("axc", "ahbgdc");
+let i = Solution_509.init().fib(3);
+let j = Solution_509.init().fib(4);
 print(i,j)
 
