092d13c0a35ada5fad7554887176796ed4022126
leerme <642459351@qq.com>
2021-11-09 15:26:53 +0800

    392判断子序列

--- leetcode/300_399/392_判断子序列/Solution_392.swift
+++ leetcode/300_399/392_判断子序列/Solution_392.swift
@@ -80,3 +80,31 @@ class Solution_392 {
 //        return true;
 //    }
 }
+//class Solution {
+//public:
+//    bool isSubsequence(string s, string t) {
+//        int n = s.size(), m = t.size();
+//
+//        vector<vector<int> > f(m + 1, vector<int>(26, 0));
+//        for (int i = 0; i < 26; i++) {
+//            f[m][i] = m;
+//        }
+//
+//        for (int i = m - 1; i >= 0; i--) {
+//            for (int j = 0; j < 26; j++) {
+//                if (t[i] == j + 'a')
+//                    f[i][j] = i;
+//                else
+//                    f[i][j] = f[i + 1][j];
+//            }
+//        }
+//        int add = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (f[add][s[i] - 'a'] == m) {
+//                return false;
+//            }
+//            add = f[add][s[i] - 'a'] + 1;
+//        }
+//        return true;
+//    }
+//};
